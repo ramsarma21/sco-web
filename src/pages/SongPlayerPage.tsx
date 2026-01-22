62d1a787c3cbb3b86f070c49afdff55912ccccb9
ramsarma21 <ramssarma@gmail.com>
2026-01-21 18:55:42 -0600

    vc

--- src/pages/SongPlayerPage.tsx
+++ src/pages/SongPlayerPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
 import type { LyricLine, Song } from "../types";
 import { sampleSong } from "../data/sampleSong";
